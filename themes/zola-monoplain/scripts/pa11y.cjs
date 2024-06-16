30897b57741dcd2ee0797dcbe3a6feab1dcfbc2a
Simon Gattner <github@0x38.de>
2024-06-16 14:51:43 +0200

    refactor(a11y): use config

--- themes/zola-monoplain/scripts/pa11y.cjs
+++ themes/zola-monoplain/scripts/pa11y.cjs
@@ -1,4 +1,11 @@
 const pa11y = require("pa11y");
+const {
+  config,
+  testPaths,
+  reportPath,
+  defaultUrl,
+} = require("../pa11y.config.cjs");
+
 // pa11y is a wrapper around axe-core and htmlcs
 // axe-core is a JavaScript library that checks for accessibility issues in your web pages
 // htmlcs is a HTML Code Sniffer for testing the accessibility of HTML source code
@@ -7,65 +14,12 @@ const pa11y = require("pa11y");
 // node scripts/pa11y.cjs https://example.com
 
 // get url from the command line
-const defaultUrl = "public/";
 let url = process.argv[2] || defaultUrl;
 
 // validate the URL
 if (url === defaultUrl) {
   console.info(`No URL provided, using default: ${defaultUrl}`);
 }
-
-// pa11y configuration
-// See: https://github.com/pa11y/pa11y?tab=readme-ov-file#configuration
-const config = {
-  includeNotices: true,
-  includeWarnings: true,
-  runners: ["axe", "htmlcs"],
-  reporter: "json",
-  standard: "WCAG2AA",
-  viewport: {
-    // iPhone SE
-    width: 375,
-    height: 667,
-    isMobile: true,
-  },
-  ignore: [
-    // Ignore Missing form labels because of markdown task lists
-    "WCAG2AA.Principle4.Guideline4_1.4_1_2.H91.InputCheckbox.Name",
-    "WCAG2AA.Principle1.Guideline1_3.1_3_1.F68",
-    "label",
-    // Ignore contrast issues because of the code theme and or default browser styles
-    "WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail",
-    "color-contrast", // axe
-    // axe frame-tested - we don't care about external iframes
-    "frame-tested",
-    // axe aria-prohibited-attr - we like to have more information on parent level
-    "aria-prohibited-attr",
-    // axe scrollable-region-focusable - we have keyboard access
-    "scrollable-region-focusable",
-  ],
-};
-
-// report path to save the report
-const reportPath = "pa11y-report";
-
-// testPaths to test
-// Need path + filename + extension to test
-const testPaths = [
-  "/index.html",
-  "/posts/index.html",
-  "/gallery/index.html",
-  "/tags/index.html",
-  "/search/index.html",
-  "/posts/ci/index.html",
-  "/posts/dictionary/index.html",
-  "/posts/icons/index.html",
-  "/posts/zola/index.html",
-  "/posts/shortcodes/index.html",
-  "/posts/emojis/index.html",
-  "/posts/markdown/index.html",
-];
-
 // remove trailing slash from url
 if (url.endsWith("/")) {
   url = url.slice(0, -1);
